d2cca720240facea4369ebb6fbf887445be3783d
Antonio <60762744+antoniokim1985@users.noreply.github.com>
2023-12-18 23:22:45 +0100

    Update script.sql

--- sql-scripts/script.sql
+++ sql-scripts/script.sql
@@ -10,25 +10,34 @@ CREATE TABLE usuarios (
     email VARCHAR(100) UNIQUE,
     password VARCHAR(100),
     rol_id INT,
-    FOREIGN KEY (rol_id) REFERENCES roles(id)
+    FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
-CREATE TABLE imagenes (
+CREATE TABLE informacion (
     id INT AUTO_INCREMENT PRIMARY KEY,
     rol_id INT,
-    FOREIGN KEY (rol_id) REFERENCES roles(id),
-    url VARCHAR(200),
+    FOREIGN KEY (rol_id) REFERENCES roles(id) ON DELETE CASCADE,
+    url_imagen VARCHAR(200),
+    url_archivo VARCHAR(200),
     titulo VARCHAR(100),
     descripcion TEXT
 );
 
-INSERT INTO roles (rol) VALUES ('niños'), ('adultos'), ('admin');
 
-INSERT INTO usuarios (nombre, apellido, email, password, rol_id) VALUES
-('Juan', 'Pérez', 'juan@example.com', 'contraseña', 1), -- 1 para 'niños'
-('María', 'González', 'maria@example.com', 'otracontraseña', 2); -- 2 para 'adultos'
+INSERT INTO roles (rol) VALUES ('admin'), ('maestro'), ('padres'), ('alumnos') ;
 
-INSERT INTO imagenes (rol_id, url, titulo, descripcion) VALUES
-(1, 'url1', 'Foto Niños 1', 'Descripción para foto de niños 1'),
-(2, 'url2', 'Foto Adultos 1', 'Descripción para foto de adultos 1');
+INSERT INTO usuarios (nombre, apellido, email, password, rol_id) 
+VALUES 
+    ('admin', 'admin', 'admin@admin.com', 'admin', 1),
+    ('Maria', 'Garcia', 'maria@example.com', 'password456', 2),
+    ('Pedro', 'Martinez', 'pedro@example.com', 'securepwd789', 3);  
+
+
+
+INSERT INTO informacion (rol_id, url_imagen, url_archivo, titulo, descripcion) 
+VALUES 
+    (1, 'url_imagen_1.jpg', 'url_archivo_1.pdf', 'Título 1', 'Descripción 1'),
+    (2, 'url_imagen_2.jpg', 'url_archivo_2.pdf', 'Título 2', 'Descripción 2'),
+    (3, 'url_imagen_3.jpg', 'url_archivo_3.pdf', 'Título 3', 'Descripción 3');
+
 
